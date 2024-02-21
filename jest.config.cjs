2f9e5d801b5930eecdf25eb683805b8a024d230e
Antonio Espinoza <aespinozareyes9@gmail.com>
2024-02-21 11:43:39 -0300

    add test and configuration for jest

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,5 @@
 module.exports = {
     testEnvironment: 'jest-environment-jsdom',
-    setupFiles: ['./jest.setup.js']
+    setupFiles: ['./jest.setup.js'],
+    transformIgnorePatterns: [], //para que no haga las transpilaciones en la parte de firebase
 }
